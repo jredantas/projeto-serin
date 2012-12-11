a56287ea0cbc1349584d4c59119b905edf38690a
brunoamuniz <brunoamuniz@fd586f87-4685-46a4-beb8-0353e4ea6077>
2012-12-11 22:45:35 +0000

    1. Adição de um novo Client, ClinicSample.java 2. Mudança do nome do ClientSample.java para VehicleSample.java

---
+++ client/trunk/serin-client/src/main/java/ClinicSample.java
@@ -0,0 +1,83 @@
+import java.net.URL;
+import java.util.List;
+
+import br.unifor.mia.serin.client.SerinClient;
+import br.unifor.mia.serin.server.clinic.Clinic;
+import br.unifor.mia.serin.server.vehicle.Vehicle;
+
+import com.hp.hpl.jena.ontology.Individual;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+public class ClinicSample {
+
+	//private static final String URL_HOST = "http://semantic-interfaces.rhcloud.com";
+
+	 private static final String URL_HOST = "http://localhost:8080/semantic";
+
+	private static final String URI_ONTOLOGY = "www.unifor.br/clinic.owl";
+
+	private static SerinClient serin;
+
+	private static OntModel model;
+
+	public static void main(String[] args) throws Exception {
+
+		URL ontology = ClinicSample.class.getClassLoader().getResource(
+				URI_ONTOLOGY.substring(URI_ONTOLOGY.indexOf('/') + 1));
+
+		model = ModelFactory.createOntologyModel();
+		model.read(ontology.openStream(), null);
+
+		// Criando as especialidades...
+		Individual specialty = model.createIndividual(Clinic.NS + "Audiology",
+				Clinic.SPECIALTY);
+		specialty.setPropertyValue(Clinic.DESCRIPTION,
+				model.createTypedLiteral("Audiology"));
+
+		// Criando os doutores...
+		Individual doctor = model.createIndividual(Clinic.NS + "Hermano_Lira",
+				Clinic.DOCTOR);
+		doctor.setPropertyValue(Clinic.DOCTORNAME,
+				model.createTypedLiteral("Hermano Lira"));
+		doctor.setPropertyValue(Clinic.isSpecializedIn, specialty);
+
+		// Criando as clinicas...
+		Individual clinic = model.createIndividual(Clinic.NS + "AUDIO_CLINIC",
+				Clinic.CLINIC);
+		clinic.setPropertyValue(Clinic.CLINICNAME,
+				model.createTypedLiteral("AUDIO CLINIC"));
+		clinic.setPropertyValue(Clinic.attendedBy, doctor);
+		clinic.setPropertyValue(Clinic.PHONENUMBER,
+				model.createTypedLiteral("34773400"));
+
+		// Criando o client...
+		serin = new SerinClient(URL_HOST, URI_ONTOLOGY);
+
+		// Adicionando um clinica...
+		boolean isCreated = serin.post(clinic);
+
+		// Informando ao usuário se o recurso foi adicionado com sucesso!
+		if (isCreated) {
+			System.out.println("Individuo criado: " + clinic);
+			model.write(System.out, "RDF/XML-ABBREV");
+		} else {
+			System.out.println("Individuo NÃO criado: " + clinic);
+		}
+
+		// GET --> Obtém o veiculo Renault Logan
+		Individual cardioClinic = serin.get(Clinic.CLINIC, "CARDIO_CLINIC");
+		System.out.println("Individuo obtido: " + cardioClinic);
+
+		// PUT --> Atualiza o veiculo Peugeot 207
+		clinic.removeAll(Clinic.PHONENUMBER);
+		clinic.setPropertyValue(Clinic.PHONENUMBER,
+				model.createTypedLiteral("43773481"));
+		serin.put(clinic);
+
+		// LIST --> Obtém uma lista de todos os veiculos
+		List<Individual> invidivuals = serin.list(Clinic.CLINIC);
+		System.out.println("Lista de Individuos: " + invidivuals);
+
+	}
+}
--- client/trunk/serin-client/src/main/java/VehicleSample.java
+++ client/trunk/serin-client/src/main/java/VehicleSample.java
@@ -8,37 +8,42 @@ import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
-public class ClientSample {
+public class VehicleSample {
 
-	private static final String URL_HOST = "http://localhost:8080/serin";
-	
+	//private static final String URL_HOST = "http://semantic-interfaces.rhcloud.com";
+
+	private static final String URL_HOST = "http://localhost:8080/semantic";
+
 	private static final String URI_ONTOLOGY = "www.unifor.br/vehicle.owl";
-	
+
 	private static SerinClient serin;
-	
+
 	private static OntModel model;
 
 	public static void main(String[] args) throws Exception {
 
-		URL ontology =
-				ClientSample.class.getClassLoader().getResource(URI_ONTOLOGY.substring(URI_ONTOLOGY.indexOf('/')+1));
-		
+		URL ontology = VehicleSample.class.getClassLoader().getResource(
+				URI_ONTOLOGY.substring(URI_ONTOLOGY.indexOf('/') + 1));
+
 		model = ModelFactory.createOntologyModel();
 		model.read(ontology.openStream(), null);
 
-		Individual peugeout207 = model.createIndividual(Vehicle.NS + "207", Vehicle.VEHICLE);
-		peugeout207.setPropertyValue(Vehicle.YEAR, model.createTypedLiteral(2010));
-		peugeout207.setPropertyValue(Vehicle.MODEL, model.createTypedLiteral("207"));
-		
+		Individual peugeout207 = model.createIndividual(Vehicle.NS + "207",
+				Vehicle.VEHICLE);
+		peugeout207.setPropertyValue(Vehicle.YEAR,
+				model.createTypedLiteral(2010));
+		peugeout207.setPropertyValue(Vehicle.MODEL,
+				model.createTypedLiteral("207"));
+
 		// CRIA CLIENTE SERIN
 		serin = new SerinClient(URL_HOST, URI_ONTOLOGY);
-		
+
 		// POST --> Adiciona o veiculo Peugeot 207
 		boolean isCreated = serin.post(peugeout207);
 
 		if (isCreated) {
 			System.out.println("Individuo criado: " + peugeout207);
-			model.write(System.out, "RDF/XML-ABBREV");	
+			model.write(System.out, "RDF/XML-ABBREV");
 		} else {
 			System.out.println("Individuo NÃO criado: " + peugeout207);
 		}
@@ -46,10 +51,11 @@ public class ClientSample {
 		// GET --> Obtém o veiculo Renault Logan
 		Individual logan = serin.get(Vehicle.VEHICLE, "Logan");
 		System.out.println("Individuo obtido: " + logan);
-		
+
 		// PUT --> Atualiza o veiculo Peugeot 207
 		peugeout207.removeAll(Vehicle.MODEL);
-		peugeout207.setPropertyValue(Vehicle.MODEL, model.createTypedLiteral("208"));
+		peugeout207.setPropertyValue(Vehicle.MODEL,
+				model.createTypedLiteral("208"));
 		serin.put(peugeout207);
 
 		// LIST --> Obtém uma lista de todos os veiculos
@@ -59,7 +65,7 @@ public class ClientSample {
 		// DELETE --> Deleta o veiculo Renault Logan
 		boolean isDeleted = serin.delete(Vehicle.VEHICLE, "Logan");
 		if (isDeleted) {
-			System.out.println("Individuo deletado: "+ Vehicle.NS +"Logan");
+			System.out.println("Individuo deletado: " + Vehicle.NS + "Logan");
 		}
 	}
 }
