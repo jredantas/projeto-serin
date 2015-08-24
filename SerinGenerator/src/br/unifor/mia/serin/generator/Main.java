88d48ed8f25ed4dddcebe0f981b80391e0440e2d
jrdantas <jrdantas@fd586f87-4685-46a4-beb8-0353e4ea6077>
2015-08-24 04:01:06 +0000

    working...

--- SerinGenerator/src/br/unifor/mia/serin/generator/Main.java
+++ SerinGenerator/src/br/unifor/mia/serin/generator/Main.java
@@ -1,34 +1,26 @@
 package br.unifor.mia.serin.generator;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Date;
-import java.util.Iterator;
 
-import javax.management.ObjectName;
-
-import com.hp.hpl.jena.graph.Graph;
-import com.hp.hpl.jena.ontology.Individual;
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.query.ReadWrite;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.ModelMaker;
-import com.hp.hpl.jena.rdf.model.NodeIterator;
-import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.vocabulary.OWL;
+import com.hp.hpl.jena.vocabulary.RDF;
 
 public class Main {
 
-	//private static final String ONTOLOGY_PATH = "/home/renato/Dropbox/Doutorado/tese/MassaDeDados/hRESTS-TC3/ontology/";
-	private static final String ONTOLOGY_PATH = "/home/09959295800/Dropbox/Doutorado/tese/MassaDeDados/hRESTS-TC3/ontology/";
+	private static final String ONTOLOGY_PATH = "/home/renato/Dropbox/Doutorado/tese/MassaDeDados/hRESTS-TC3/ontology/teste/";
+	//private static final String ONTOLOGY_PATH = "/home/09959295800/Dropbox/Doutorado/tese/MassaDeDados/hRESTS-TC3/ontology/";
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		System.out.println("Discovery agent started...");
+		System.out.println("SERIN Generator started...");
 		
 		/**
 		 * Construir lista de ontologias para anotar:
@@ -43,75 +35,47 @@ public class Main {
 		File directory = new File(Main.ONTOLOGY_PATH);
 	    File[] files = directory.listFiles();
 	    Model serin = FileManager.get().loadModel("http://www.activeontology.com.br/serin.owl");
-	    /*Resource r = serin.getResource("http://www.w3.org/2002/07/owl#AnnotationProperty");
-       	//System.out.println("1===============================================");
-       	StmtIterator rdf = r.listProperties();
-       	while(rdf.hasNext()) {
-       		Statement n = rdf.next();
-       		String s = n.toString();
-	      // 	System.out.println(s);
-         }
+	    ResIterator annotations = serin.listSubjectsWithProperty(RDF.type, OWL.AnnotationProperty);
+       	//while(annotations.hasNext()) {
+       	//	Resource r3 = serin.getResource(annotations.next().toString());
+       	//	System.out.print("Anotação SERIN:");
+	    //  	System.out.println(r3.toString());
+        // }
        	//System.out.println("===============================================");
-       	//System.out.println("===============================================");
-       	//System.out.println("===============================================");
-       	NodeIterator serinNodes = serin.listObjects();
-       	while(serinNodes.hasNext()) {
-       		RDFNode n = serinNodes.next();
-       		String s = n.toString();
-	      // 	System.out.println(s);
-         }
-       	//System.out.println("===============================================");
-       	ResIterator serinresources = serin.listSubjects();
-       	while(serinresources.hasNext()) {
-       		RDFNode n = serinresources.next();
-       		String s = n.toString();
-	       	System.out.println(s);
-         }
-       	//System.out.println("===============================================");
-	   */
+	    
+	    
+	    
        	Date d1 = new Date();
 	    System.out.println(d1);
 	    //for ( int i = 0; i < files.length; i++ ){
 	    	int i = 0;
 	       	System.out.println(Main.ONTOLOGY_PATH+files[i].getName());
-	       	Model m = FileManager.get().loadModel( Main.ONTOLOGY_PATH+files[i].getName() );
-	       	//OntModel m = ModelFactory.createOntologyModel();
-/*
-		    Resource r2 = m.getResource("http://127.0.0.1/ontology/ApothecaryOntology.owl#PhysicianID");
-	       	//System.out.println("2===============================================");
-	       	StmtIterator rdf2 = r2.listProperties();
-	       	while(rdf2.hasNext()) {
-	       		Statement n = rdf2.next();
-	       		String s = n.toString();
-		      // 	System.out.println(s);
-	         }
-	       	//System.out.println("===============================================");
-	       	//System.out.println("===============================================");
-	       	//System.out.println("===============================================");
-*/
+	       	Model model = FileManager.get().loadModel( Main.ONTOLOGY_PATH+files[i].getName() );
 	       	
-	       	
-	       	ResIterator resources = m.listSubjects();
-	       	//StmtIterator resources = m.listStatements();
-	       	//NodeIterator resources = m.listObjects();
+	       	model.add(serin);
+
+	       	ResIterator resources = model.listSubjectsWithProperty(RDF.type, OWL.Class);
 	       	int cont = 1;
 	       	while(resources.hasNext()) {
-	       		//RDFNode n = resources.next();
-	       		//String s = n.toString();
-	       		Resource r3 = m.getResource(resources.nextResource().toString());
-	       		//Resource r3 = resources.nextStatement().getSubject().as();
-		       		System.out.println("Recurso "+cont+":");
-			       	System.out.println(r3.toString());
-		       		cont++;
-		       	//StmtIterator rdf3 = r3.listProperties();
-		       	//while(rdf3.hasNext()) {
-		       	//	Statement n = rdf3.nextStatement();
-			     //  	System.out.println(n.toString());
-		         //}
-	       		System.out.println("--------------------------------");
-		       	
+	       		Resource r3 = model.getResource(resources.next().toString());
+	       		//System.out.print("Recurso "+cont+":");
+		       	//System.out.println(r3.toString());
+	       		//Statement stm = model.add;
+	       		cont++;
 	         }
 	       	System.out.println("===============================================");
+	       	
+	       	try {
+				FileOutputStream output = new FileOutputStream(Main.ONTOLOGY_PATH+files[i].getName());
+				model.write(output);
+				output.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	       	
 	       	//String a = m.getResource("http://127.0.0.1/ontology/ApothecaryOntology.owl#Physician_hasID").getNameSpace();
 	       	//System.out.println(a);
