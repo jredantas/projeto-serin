344f37ad0a5c3176d9f065e8643693f0a705db00
jrdantas <jrdantas@fd586f87-4685-46a4-beb8-0353e4ea6077>
2015-08-24 14:54:39 +0000

    working...

--- SerinGenerator/src/br/unifor/mia/serin/generator/Main.java
+++ SerinGenerator/src/br/unifor/mia/serin/generator/Main.java
@@ -4,11 +4,17 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
+import java.util.List;
 
+import com.hp.hpl.jena.ontology.AnnotationProperty;
+import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.vocabulary.OWL;
 import com.hp.hpl.jena.vocabulary.RDF;
@@ -34,40 +40,56 @@ public class Main {
 		 */
 		File directory = new File(Main.ONTOLOGY_PATH);
 	    File[] files = directory.listFiles();
-	    Model serin = FileManager.get().loadModel("http://www.activeontology.com.br/serin.owl");
+	    //Model serin = FileManager.get().loadModel("http://www.activeontology.com.br/serin.owl");
+	    InputStream in = FileManager.get().open("http://www.activeontology.com.br/serin.owl");
+	    if (in == null) {
+			throw new IllegalArgumentException("File: " + in + " not found");
+		}
+	    OntModel serin = ModelFactory.createOntologyModel();
+	    serin.read(in, null);
 	    ResIterator annotations = serin.listSubjectsWithProperty(RDF.type, OWL.AnnotationProperty);
-       	//while(annotations.hasNext()) {
-       	//	Resource r3 = serin.getResource(annotations.next().toString());
-       	//	System.out.print("Anotação SERIN:");
-	    //  	System.out.println(r3.toString());
-        // }
-       	//System.out.println("===============================================");
-	    
-	    
-	    
+       	while(annotations.hasNext()) {
+       		Resource r3 = serin.getResource(annotations.next().toString());
+       		System.out.print("Anotação SERIN:");
+	      	System.out.println(r3.toString());
+         }
+       	System.out.println("===============================================");
+	    	    
        	Date d1 = new Date();
 	    System.out.println(d1);
 	    //for ( int i = 0; i < files.length; i++ ){
 	    	int i = 0;
 	       	System.out.println(Main.ONTOLOGY_PATH+files[i].getName());
-	       	Model model = FileManager.get().loadModel( Main.ONTOLOGY_PATH+files[i].getName() );
+	       	//Model model = FileManager.get().loadModel( Main.ONTOLOGY_PATH+files[i].getName() );
 	       	
-	       	model.add(serin);
+	       	InputStream in2 = FileManager.get().open(Main.ONTOLOGY_PATH+files[i].getName());
+		    if (in2 == null) {
+				throw new IllegalArgumentException("File: " + in2 + " not found");
+			}
+		    OntModel model = ModelFactory.createOntologyModel();
+		    model.read(in2, null);
+		    	       	
+	       	//model.add(serin);
+	       	
+	       	//final AnnotationProperty get = model.createAnnotationProperty( "http://www.activeontology.com.br/serin.owl#get" );
 
 	       	ResIterator resources = model.listSubjectsWithProperty(RDF.type, OWL.Class);
 	       	int cont = 1;
+	       	List<Statement> includes = null;
 	       	while(resources.hasNext()) {
 	       		Resource r3 = model.getResource(resources.next().toString());
-	       		//System.out.print("Recurso "+cont+":");
-		       	//System.out.println(r3.toString());
-	       		//Statement stm = model.add;
+	       		System.out.print("Recurso "+cont+":");
+		       	System.out.println(r3.toString());
+	       		
+	       		//Statement stm = r3.addProperty(arg0, arg1);
+	       		//includes.add(stm);
 	       		cont++;
 	         }
 	       	System.out.println("===============================================");
-	       	
+	       	model.add(includes);
 	       	try {
 				FileOutputStream output = new FileOutputStream(Main.ONTOLOGY_PATH+files[i].getName());
-				model.write(output);
+				//model.write(output);
 				output.close();
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
